97faca00d9b1a8096926784549d5b1f2f7f15de5
KiraElric <lissette.urso@gmail.com>
2021-03-22 22:30:21 -0300

    Desafio Autenticacion manual

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Successfully sign in'
+    else
+      flash.now[:notice] = 'Email or password invalid'
+      render action: :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Signed out successfully'
+  end
+end
